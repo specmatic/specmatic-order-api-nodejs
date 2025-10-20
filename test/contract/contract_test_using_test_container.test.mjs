62c58d2cf56e1382a76eafc7ba0d876e149f2604
Sufiyan <StarKhan6368@gmail.com>
2025-10-20 12:58:43 +0530

    Updated to Specmatic 2.27.0, add testContainer setup

---
+++ test/contract/contract_test_using_test_container.test.mjs
@@ -0,0 +1,52 @@
+const { GenericContainer } = await import("testcontainers");
+import path from "node:path";
+import { startAppServer, stopAppServer } from "./util/app.server";
+import { Wait } from "testcontainers";
+
+const APP_PORT = 8081;
+const describeOrSkipInCI = process.env.CI ? describe.skip : describe;
+
+describeOrSkipInCI("Contract Tests", () => {
+    /**
+     * @type {import("testcontainers").GenericContainer}
+     */
+    let specmaticTestContainer;
+    /**
+     * @type {any}
+     */
+    let appServer;
+
+    beforeAll(async () => {
+        appServer = await startAppServer(APP_PORT);
+        specmaticTestContainer = new GenericContainer("specmatic/specmatic")
+            .withCommand(["test", "--host", "host.docker.internal", "--port", APP_PORT.toString()])
+            .withEnvironment({ SPECMATIC_GENERATIVE_TESTS: "true" })
+            .withBindMounts([
+                { source: path.resolve("specmatic.yaml"), target: "/usr/src/app/specmatic.yaml" },
+                { source: path.resolve("build/reports/specmatic"), target: "/usr/src/app/build/reports/specmatic" }
+            ])
+            .withLogConsumer(stream => {
+                stream.on("data", process.stdout.write);
+                stream.on("err", process.stderr.write);
+            })
+            .withWaitStrategy(Wait.forLogMessage("Tests run:"));
+    }, 600_000);
+
+    afterAll(async () => {
+        await stopAppServer(appServer);
+    });
+
+    test("Run contract tests", async () => {
+        let testContainerLogs = "";
+        const container = await specmaticTestContainer.start();
+        const stream = await container.logs();
+
+        await new Promise(resolve => {
+            stream.on("data", chunk => testContainerLogs += chunk.toString());
+            stream.on("err", chunk => testContainerLogs += chunk.toString());
+            stream.on("close", resolve);
+        });
+
+        expect(testContainerLogs).toMatch(/Failures:\s*0/);
+    }, 600_000);
+});
